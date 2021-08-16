706c6250ec795166487a346213b52dbc325bcf0a
Farkhod Zokhidov <farafuri77@gmail.com>
2021-08-16 09:55:04 +0500

    Burger

--- src/Foods/Menu/index.jsx
+++ src/Foods/Menu/index.jsx
@@ -1,12 +1,60 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import './Menu.css'
-export default class Menu extends Component {
+import {data} from '../mock'
+import Order from '../orders/order'
 
-    render(){
-        return  <div className='menu-con'>Menu</div>
-      
 
-        
+export default class menu extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            lists: data,
+            selected:[],
+            total: 0
+        }
     }
+    render() {
+        const onselect = (item)=>{
+            const selected = [...this.state.selected, item]
+            this.setState({selected})  
+            selected.forEach(item=>{
+                this.setState({total:this.state.total + item.price})
+            })
+        }
 
+        const onDelete=(id)=>{
+            console.log(id, 'select');
+        }
+
+        return (
+            <div className='menu-con'>
+                <div className='category'>
+                    {this.state.lists.map((value)=>{
+                        return(
+                            <div>
+                                <h1>{value.category}</h1>
+                                <div className='list'>
+                                    {
+                                        value?.list.map((item)=>{
+                                            return(
+                                                <div onClick={()=>onselect(item)} className='list-item'>
+                                                    <img src={item.img} alt="" />
+                                                    <h1>{item.title}</h1>
+                                                    <h3>{item.price}sum</h3>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+
+                            </div>
+                        ) 
+                    })}
+                </div>
+                <Order 
+                onDelete = {(id)=>onDelete(id)}
+                total = {this.state.total} selected = {this.state.selected} />
+            </div>
+        )
+    }
 }
--- src/Foods/Order/index.jsx
+++ src/Foods/Order/index.jsx
@@ -1,12 +1,35 @@
-import React, {Component} from 'react';
-import './Order.css'
-export default class Order extends Component {
+import React, { Component } from 'react'
+import './order.css'
+import {data} from '../mock'
 
-    render(){
-        return  <div className='order-con'>Order</div>
-      
-
-        
+export default class order extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            lists: data,
+        }
     }
-
+    render() {
+        return (
+            <div className='order-con'>
+                <h1>Selected orders list</h1>
+                
+                    <div className='selected-list'>
+                        {this.props?.selected?.map((value)=>(
+                        <div >  
+                            <h1>{value.title}</h1>
+                            <h3>{value.price}</h3>
+                            <button onClick={()=>this.props.onDelete(value.id)}>delete</button>
+                        </div>  
+                        ))}
+                    </div>
+                <div className='total'>
+                    
+                    <h1>Total: {this.props.total} sum</h1>
+                    
+                </div>
+            </div>
+        )
+    }
 }
+
