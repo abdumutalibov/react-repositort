ebe22487912b39c2e51f35aa77805473b87fb362
Abduali <dreyfus1309@gmail.com>
2021-09-08 12:49:39 +0500

    Figma 22

--- src/Figma2/Navbar.js
+++ src/Figma2/Navbar.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 import './Navbar.css'
-import { Card, Get, Home, Hoow, Img1, Img2, Lets, Lucos, Main, Text1, Text2, Text3, Text4 } from './style'
+import { Card, Crkle, Get,
+     Home, Hoow,
+      Img1, Img2, 
+      Img3, 
+      Img4, 
+      Img5, 
+      Img6, 
+      Lets, Linee, Livin, Lucos, 
+      Main, Our, Paset, Pasete, Pasete2, Spice, 
+      Text1, Text10, Text11, Text12, Text13, Text2,
+       Text3, Text4, Text5, Text6, Text7, Text8, Text9 } from './style'
 import Uy from './img/uy.png'
 import Rang from './img/rang.png'
 import Kres from './img/kres.png'
+import Devan from './img/devan.png'
+import Erow from './img/errow.png'
+import Pasha from './img/pasha.png'
+import Room from './img/room.png'
+import Room2 from './img/room2.png'
 
 export default class Navbar extends Component {
     render() {
@@ -40,7 +55,58 @@ export default class Navbar extends Component {
                </Img1></Lets> 
                 
                <Hoow>How it works
+                   <Spice> Spice up your space even </Spice>
+                   <Spice>  simpler than you think</Spice>
+                   <Img3><img src={Devan}/> 
+                  
+                   <Text5>
+                       <Text6>We provide design and build </Text6>
+                       <Text6>for commercial building, </Text6>
+                       <Text6>interior and furniture to improve </Text6>
+                       <Text6>the better life.</Text6>
+                       <Text7>SEE VIDEO</Text7>
+                   </Text5>
+                
+                    <Crkle><img src={Erow}/></Crkle>
+                   </Img3>
+                   <Img4><img src={Pasha}/>
+                   
+                  
+                   <Text8>
+                       <Text9>Our missino is to </Text9>
+                       <Text9>deliver aesthetic visual </Text9>
+                       <Text9>for your home
+                           
+                       </Text9>
+                           <Pasete></Pasete>
+                       <Text10>By applying bacis psychology principles, </Text10>
+                       <Text10>we are going to explain how perception </Text10>
+                       <Text10>works and how we can use this understanding </Text10>
+                       <Text10>to maximise the space that we occupy in </Text10>
+                       <Text10>our homes.</Text10>
+                       
+                   </Text8>
+                   </Img4>
+                   <Our>Our project</Our>
+                   <Text11>Introducing our first official project
+                       
+                   </Text11>
+                   <Linee></Linee>
+                   <Linee></Linee>
+                   
+                   <Pasete2></Pasete2>
+                   
+                   <Livin>Project Livingroom</Livin>
+                   <Text12>A simple guide to create the perfect livingro-</Text12>
+                   <Text12> om for . Night Watc works beautifully in a wi- </Text12>
+                   <Text12>de gamut of arrangements. </Text12>
+                   <Img5><img src={Room}/></Img5>
+                   <Img6><img src={Room2}/></Img6>
+                   <Text13>Project workspace</Text13>
+
+
               </Hoow>
+              
                </Get>
                
             </div>
--- src/Figma2/style.js
+++ src/Figma2/style.js
@@ -162,3 +162,225 @@ text-align: center;
 margin-top:350px;
 color: #1C2323;
 `
+export const Spice =styled.div`
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 30px;
+line-height: 45px;
+text-align: center;
+
+color: #404242;
+`
+export const Img3 = styled.div`
+margin-top: 70px; 
+display: flex;
+padding-bottom:150px;
+`
+export const Crkle =styled.div`
+width: 1248px;
+height: 1248px;
+/* left: 1165px;
+top: 1640px; */
+
+background: rgba(255, 255, 255, 0.1);
+border-radius: 65px;
+background-color:red;
+position:relative;
+top:190px;
+right: 600px;
+display:flex;
+justify-content: center;
+align-items: center;
+
+width: 100px;
+height: 100px;
+/* left: 1179px;
+top: 1654px; */
+
+background: #FFFFFF;
+border-radius: 65px;
+`
+export const Text5=styled.div`
+width: 549px;
+height: 350px;
+/* left: 1229px;
+top: 1458px; */
+
+background: #CCF0E6;
+border-radius: 0px 10px 10px 0px;
+
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 30px;
+
+color: #486958;
+
+/* position :relative; */
+/* margin-right:60px */
+/* 
+/* flex-direction: column; */
+`
+export const Text6=styled.div`
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 30px;
+
+color: #486958;
+display :flex;
+justify-content: flex-start;
+align-items: center;
+text-align: center;
+position:relative;
+top:50px;
+padding-left: 149px;
+/* padding-top:50px; */
+
+`
+export const Text7 =styled.div`
+
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 20px;
+line-height: 30px;
+/* identical to box height */
+display:flex;
+justify-content:flex-start;
+padding-left: 149px;
+
+margin-top: 150px;
+color: #486958;
+
+`
+export const Img4 =styled.div`
+display:flex;
+padding-bottom: 120px;
+
+`
+ export const Text8=styled.div`
+
+ `
+ export const Text9=styled.div`
+  font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 40px;
+line-height: 60px;
+margin-left:145px;
+color: #1C2323;
+display: flex;
+justify-content: flex-start;
+line-height: 15px;
+padding-top:40px;
+ `
+ export const Text10=styled.div`
+padding-top:40px;
+ 
+font-family: Poppins;
+font-style: normal;
+font-weight: 500;
+font-size: 30px;
+line-height: 4px;
+
+color: #486958;
+display:flex; 
+ justify-content: flex-start;
+ margin-left:145px;
+
+ `
+ export const Pasete =styled.div`
+ padding: 50px;
+ `
+ export const Our =styled.div`
+ font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 70px;
+line-height: 105px;
+/* identical to box height */
+
+/* text-align: center; */
+
+color: #1C2323;
+
+ `
+ export const Text11 =styled.div`
+ font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 30px;
+line-height: 45px;
+/* identical to box height */
+
+text-align: center;
+padding-top:20px;
+padding-bottom: 51px;;
+color: #404242;
+ `
+ export const Linee=styled.div`
+ width: 135px;
+height: 0px;
+left: 906px;
+top: 2949px;
+margin-bottom:10px;
+border: 5px solid #58A9A5;
+display: flex;
+justify-content: center;
+align-items:center;
+margin-left:820px;
+background-color: #58A9A5;
+ `
+ export const Pasete2 =styled.div`
+ padding: 70px;
+ `
+ export const Livin =styled.div`
+ 
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 40px;
+line-height: 60px;
+/* identical to box height */
+display:flex;
+justify-content: flex-start;
+padding-bottom:5px;
+color: #58A9A5;
+ `
+ export const Text12=styled.div`
+ font-family: Poppins;
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 30px;
+display:flex;
+justify-content: flex-start;
+color: #486958;
+ `
+ export const Img5=styled.div`
+ display:flex;
+
+ padding-top: 35px;
+ `
+ export const Img6=styled.div`
+ position:relative;
+ top:-650px;
+ `
+ export const Text13=styled.div` 
+ 
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 40px;
+line-height: 60px;
+/* identical to box height */
+
+
+color: #58A9A5;
+ `
